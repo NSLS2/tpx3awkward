d30df5d6229e3f077d89c4f71115b0dbb6c99fd7
Joseph Hanrahan <jhanrahan@bnl.gov>
2026-06-23 10:53:20 -0400

    add decoding tdc test

--- tests/processing/test_decoding.py
+++ tests/processing/test_decoding.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+import pandas as pd
+
 from tpx3awkward.processing.decoding import decode_tpx3_binary
 from tpx3awkward.processing.files import raw_as_numpy
 
@@ -21,3 +23,23 @@ def test_decode_tpx3_binary_serval_4_missing_messages(capsys):
     decode_tpx3_binary_capture = capsys.readouterr()
     # numba doesn't support raising errors, so we print error messages
     assert "Missing messages!" not in decode_tpx3_binary_capture.out
+
+
+def test_decode_tpx3_binary_tdc():
+    path_to_raw_data = RAW_DATA_DIR / "tdc/"
+    raw_tpx3_file_paths = sorted([p for p in path_to_raw_data.glob("*") if p.is_file() and ".tpx3" in str(p)])
+
+    tdc_dfs = []
+    for raw_tpx3_file_path in raw_tpx3_file_paths:
+        binary = raw_as_numpy(raw_tpx3_file_path)
+        _, tdc_df = decode_tpx3_binary(binary)
+        tdc_dfs.append(tdc_df)
+
+    concat_tdc_df = pd.concat(tdc_dfs, ignore_index=True)
+    required = {"tdc_t_ns", "tdc_type", "tdc_chip"}
+    assert required.issubset(concat_tdc_df.columns)
+
+    # time should be monotonically increasing
+    # tdc events come in bunches per chip, so only check for one
+    concat_tdc_df_chip_0 = concat_tdc_df.loc[concat_tdc_df["tdc_chip"] == 0]
+    assert concat_tdc_df_chip_0["tdc_t_ns"].is_monotonic_increasing
